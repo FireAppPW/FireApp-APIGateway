b9b3a09488b116ea1a4d97faca41095d72d1cced
Truong Giang Do <giangh14cqt@gmail.com>
2023-05-25 00:48:30 +0200

    Add ci cd

---
+++ src/main/java/pw/ersms/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package pw.ersms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // Eureka Discovery Client
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
--- src/test/java/pw/ersms/gateway/GatewayApplicationTests.java
+++ src/test/java/pw/ersms/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package pw.ersms;
+package pw.ersms.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
