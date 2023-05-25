dbbeeb0758a01a211d55abc366199de3b8ea2ddf
Truong Giang Do <giangh14cqt@gmail.com>
2023-05-25 19:23:13 +0200

    Disable

--- src/main/java/pw/ersms/gateway/GatewayApplication.java
+++ src/main/java/pw/ersms/gateway/GatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient // Eureka Discovery Client
+//@EnableDiscoveryClient // Eureka Discovery Client
 public class GatewayApplication {
 
 	public static void main(String[] args) {
