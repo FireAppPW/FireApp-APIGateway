4abe552d1252b00ef33d694ffe95eb328c3c9da9
Truong Giang Do <giangh14cqt@gmail.com>
2023-06-08 21:02:04 +0200

    Add cord

--- src/main/java/pw/ersms/gateway/GatewayApplication.java
+++ src/main/java/pw/ersms/gateway/GatewayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient // Eureka Discovery Client
 public class GatewayApplication {
 
 	public static void main(String[] args) {
